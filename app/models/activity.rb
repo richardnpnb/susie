f7e910c969f0ba80bb66ad1f9abe18ea6cdee62d
richardnpnb <richardn@npnb.co.uk>
2015-05-29 21:33:16 +0100

    #00440 - setting up self referencing parent Activities in formtastic

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,10 @@ class Activity < ActiveRecord::Base
 
 	# added line below #00425
 	belongs_to :activity_type
-	belongs_to :activity
+	
+    # #00440 - next 2 lines: setting up self referencing in formtastic
+	belongs_to :parent_activity, :foreign_key => "parent_id", :class_name => "Activity"
+    has_many :activities, :foreign_key => "parent_id"
 
 	validates_presence_of :name
 
