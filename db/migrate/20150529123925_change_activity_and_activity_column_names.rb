2bca69dccf36ae7c45a40bd76e874057a39cf325
richardnpnb <richardn@npnb.co.uk>
2015-06-05 22:40:57 +0000

    Commit for Heroku

--- db/migrate/20150529123925_change_activity_and_activity_column_names.rb
+++ db/migrate/20150529123925_change_activity_and_activity_column_names.rb
@@ -1,6 +1,6 @@
 class ChangeActivityAndActivityColumnNames < ActiveRecord::Migration
   def change
-  	rename_column :activity_types, :activity_type_name, :name 
-    rename_column :activities, :activity_name, :name 
+  	rename_column :activity_type, :activity_type_name, :name 
+    rename_column :activity, :activity_name, :name 
   end
 end
