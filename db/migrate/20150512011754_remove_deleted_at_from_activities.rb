0af5761e1d675364f98f82fd65201ff1530d4444
richardnpnb <richardn@npnb.co.uk>
2015-06-05 22:53:30 +0000

    Commit for Heroku

--- db/migrate/20150512011754_remove_deleted_at_from_activities.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDeletedAtFromActivities < ActiveRecord::Migration
-  def change
-    remove_index :activities, :deleted_at
-    remove_column :activities, :deleted_at, :datetime
-  end
-end
