45f35be1e87d4960b5f8af5dea8183e8e0fe1f9a
richardnpnb <richardn@npnb.co.uk>
2015-06-05 23:54:47 +0000

    Commit for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+activity_types = ActivityType.create([{ name: 'System2' }])
+Activity.create(name: 'Root2', activity_type: activity_types.first)
